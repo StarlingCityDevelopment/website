bc7be692ee588a57524c6ad9061ee50e8968160f
CodexisPhantom <contact@codexis.dev>
2025-02-15 12:06:11 +0100

    chore: new deployment

--- svelte.config.js
+++ svelte.config.js
@@ -5,12 +5,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : '/website'
-		}
+		adapter: adapter()
 	}
 };
 
